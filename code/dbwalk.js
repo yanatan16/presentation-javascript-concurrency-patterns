d49ba761e660e0f98511509386c2084aee1541f8
Jon Eisen <jon@metricfeat.com>
2014-09-22 09:35:51 -0600

    add async map

---
+++ code/dbwalk.js
@@ -0,0 +1,22 @@
+// STARTASYNC OMIT
+function findFriends(userId, callback) {
+  findUser(userId, function (err, friends) {
+    if (err) return callback(err)
+    async.map(user.friends, findUser, callback) // HL
+  })
+}
+function findUser(userId, callback) {
+  db.users.findOne({_id: userId}, callback)
+}
+// ENDASYNC OMIT
+
+// STARTPROMISE OMIT
+function findFriends(userId) {
+  return findUser(userId).then(function (user) {
+    return Q.all(user.friends.map(findUser)) // HL
+  })
+}
+function findUser(userId) {
+  return Q.denodify(db.users.findOne.bind(db.users), {_id: userId}) // HL
+}
+// ENDPROMISE OMIT
